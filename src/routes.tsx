e5acbb31d55a9140ffc363a9c3e8c99269fbe9f1
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-07 17:37:54 -0300

    Add font-family e eliminado imports desnecessarios

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,5 @@
 import { Routes, Route } from "react-router";
 import { Home } from "./pages/Home";
-import { Login } from "./pages/Login";
-import { Register } from "./pages/Registro";
 
 const Rotas = () => {
   return (
