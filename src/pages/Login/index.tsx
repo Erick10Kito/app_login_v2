bff784a22c201f9faac7f40112f1b6ca33c5ea1d
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-07 11:07:46 -0300

    Adicionado pagina de login e Registro

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <>
+      <div className="px-4">
+        <h1>Ola</h1>
+      </div>
+    </>
+  );
+}
+
+export { Login };
---
+++ src/pages/Registro/index.tsx
@@ -0,0 +1,11 @@
+function Register() {
+  return (
+    <>
+      <div className="px-4">
+        <h1>Ola</h1>
+      </div>
+    </>
+  );
+}
+
+export { Register };
