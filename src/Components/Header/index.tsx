f6fc550c2efd9e6addb5e0c8dca583878081c38b
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-07 11:20:26 -0300

    Atualizado layout do Header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="flex justify-center">
-      <div className="w-full bg-black pt-8 px-4">
+      <div className="w-full bg-gray-900 pt-8 px-4">
         <div className="mb-8">
           <h1 className="text-white">Bem vindo!</h1>
           <p className="text-white">
@@ -11,11 +11,17 @@ function Header() {
           </p>
         </div>
         <div className="flex justify-between">
-          <Link to="/" className="h-12 items-center w-6/12 flex justify-center">
-            <h3 className="text-white">Login</h3>
+          <Link
+            to="/"
+            className="duration-[0.5s] text-white h-12 items-center w-6/12 flex justify-center hover:bg-white hover:text-gray-900"
+          >
+            <h3 className="">Login</h3>
           </Link>
-          <Link to="/" className="h-12 items-center w-6/12 flex justify-center">
-            <h3 className="text-white">Register</h3>
+          <Link
+            to="/"
+            className="duration-[0.5s] text-white h-12 items-center w-6/12 flex justify-center hover:bg-white hover:text-gray-900"
+          >
+            <h3 className="">Register</h3>
           </Link>
         </div>
       </div>
