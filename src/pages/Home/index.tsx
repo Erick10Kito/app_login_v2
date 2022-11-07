0ea9c79f81e6fe9d25df8fb1be50a808fa897e16
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-07 17:39:27 -0300

    estilização das paginas Home,Login e Registro

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,33 +7,37 @@ function Home() {
     "login" | "register"
   >("login");
   return (
-    <div>
-      <div className="flex justify-center">
-        <div className="w-full bg-gray-900 pt-8 px-4">
-          <div className="mb-8">
-            <h1 className="text-white">Bem vindo!</h1>
-            <p className="text-white">
-              Selecione uma opção e continue o processo!
-            </p>
-          </div>
-          <div className="flex justify-between">
-            <button
-              onClick={() => setSelectedLoginOrRegister("login")}
-              className="duration-[0.5s] text-white h-12 items-center w-6/12 flex justify-center hover:bg-white hover:text-gray-900"
-            >
-              <h3 className="">Login</h3>
-            </button>
-            <button
-              onClick={() => setSelectedLoginOrRegister("register")}
-              className="duration-[0.5s] text-white h-12 items-center w-6/12 flex justify-center hover:bg-white hover:text-gray-900"
-            >
-              <h3 className="">Register</h3>
-            </button>
+    <div className="flex justify-center items-center h-screen bg-gray-700">
+      <div className="w-[700px]">
+        <div className="flex justify-center">
+          <div className="w-full bg-gray-900 pt-8">
+            <div className="mb-8 px-4">
+              <h1 className="text-white font-['Titillium_Web'] text-[40px]">
+                Bem vindo!
+              </h1>
+              <p className="text-white font-['Titillium_Web'] text-[18px]">
+                Selecione uma opção e continue o processo!
+              </p>
+            </div>
+            <div className="flex justify-between">
+              <button
+                onClick={() => setSelectedLoginOrRegister("login")}
+                className="duration-[0.5s] text-white h-12 items-center w-6/12 flex justify-center hover:bg-gray-200 hover:text-gray-900 font-['Titillium_Web'] text-[20px]"
+              >
+                <h3 className="">Login</h3>
+              </button>
+              <button
+                onClick={() => setSelectedLoginOrRegister("register")}
+                className="duration-[0.5s] text-white h-12 items-center w-6/12 flex justify-center hover:bg-gray-200 hover:text-gray-900 font-['Titillium_Web'] text-[20px]"
+              >
+                <h3 className="">Register</h3>
+              </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div>
-        {selectedLoginOrRegister === "login" ? <Login /> : <Register />}
+        <div className="pt-12 pb-12 bg-gray-200">
+          {selectedLoginOrRegister === "login" ? <Login /> : <Register />}
+        </div>
       </div>
     </div>
   );
--- src/pages/Registro/index.tsx
+++ src/pages/Registro/index.tsx
@@ -1,10 +1,31 @@
+import { ButtonGoogleLogin } from "../../Components/ButtonGoogleLogin/buttonGoogleLogin";
+import { InputApp } from "../../Components/Input/input";
+
 function Register() {
   return (
-    <>
-      <div className="px-4">
-        <h1>Ola Registro</h1>
+    <div>
+      <h1 className="text-black text-center mb-5 font-['Titillium_Web'] text-[25px]">
+        Registre-se com:
+      </h1>
+      <div>
+        <form className="bg-[transparent] flex flex-col items-center gap-[25px]">
+          <InputApp type="text" name="email" id="email" placeholder="email" />
+          <InputApp
+            type="password"
+            name="password"
+            id="password"
+            placeholder="password"
+          />
+        </form>
       </div>
-    </>
+
+      <p className="font-['Titillium_Web'] text-[25px] text-center my-[20px]">
+        Ou
+      </p>
+      <div className="flex justify-center">
+        <ButtonGoogleLogin />
+      </div>
+    </div>
   );
 }
 
