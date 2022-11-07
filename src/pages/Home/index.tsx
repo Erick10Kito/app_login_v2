7fb054339bae23f81abf6ef6c7aa752133739bf5
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-07 11:37:17 -0300

    Paina home com novas opções

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,25 @@
 function Home() {
   return (
-    <>
-      <div className="px-4">
-        <h1>Ola</h1>
+    <div>
+      <div className="flex justify-center">
+        <div className="w-full bg-gray-900 pt-8 px-4">
+          <div className="mb-8">
+            <h1 className="text-white">Bem vindo!</h1>
+            <p className="text-white">
+              Selecione uma opção e continue o processo!
+            </p>
+          </div>
+          <div className="flex justify-between">
+            <button className="duration-[0.5s] text-white h-12 items-center w-6/12 flex justify-center hover:bg-white hover:text-gray-900">
+              <h3 className="">Login</h3>
+            </button>
+            <button className="duration-[0.5s] text-white h-12 items-center w-6/12 flex justify-center hover:bg-white hover:text-gray-900">
+              <h3 className="">Register</h3>
+            </button>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
 
