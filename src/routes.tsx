5e539dc190049a353d3777b0a4637d9315bde12e
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-07 10:19:15 -0300

    Rotas e pagina de Home adicionadas

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router";
+import { Home } from "./pages/Home";
+
+const Rotas = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+};
+
+export default Rotas;
