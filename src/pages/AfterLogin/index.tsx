c3d537a1c7808fa7e06ea0ffbde7429f1c4ed8a1
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-09 12:10:09 -0300

    Estilização pagina do usuario

--- src/pages/AfterLogin/index.tsx
+++ src/pages/AfterLogin/index.tsx
@@ -1,10 +1,27 @@
+import { useContext } from "react";
 import { HeaderAfterLogin } from "../../Components/HeaderAfterLogin";
+import { AuthContext } from "../../contexts/AuthContext";
 
 function AfterLogin() {
+  const { user } = useContext(AuthContext);
   return (
     <div className="bg-gray-700">
       <HeaderAfterLogin />
-      <h1>ola</h1>
+      <div>
+        <div className="h-[350px] flex items-center px-4">
+          <div>
+            <img
+              className="h-48 border-solid border-2 border-white rounded-[10px]"
+              src={user.avatar}
+            />
+          </div>
+          <div>
+            <h2>{user.name}</h2>
+            <p>{user.mail}</p>
+          </div>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
