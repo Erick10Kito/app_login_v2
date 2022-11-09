96e3be542e413b96ad319e7ef4a5e248c2cdf1e3
Erick de Bem Campos <erickcamposbem69@gmail.com>
2022-11-09 14:28:43 -0300

    Pagina do Usuario estilizada

--- src/pages/AfterLogin/index.tsx
+++ src/pages/AfterLogin/index.tsx
@@ -8,19 +8,34 @@ function AfterLogin() {
     <div className="bg-gray-700">
       <HeaderAfterLogin />
       <div>
-        <div className="h-[350px] flex items-center px-4">
-          <div>
-            <img
-              className="h-48 border-solid border-2 border-white rounded-[10px]"
-              src={user.avatar}
-            />
+        <div className="px-4 flex items-center justify-between">
+          <div className="h-[350px] flex items-center w-[50%]">
+            <div>
+              <img
+                className="h-48 border-solid border-2 border-white rounded-[10px]"
+                src={user.avatar}
+              />
+            </div>
+            <div className="ml-8">
+              <h2 className="text-white text-3xl font-['Titillium_Web']">
+                {user.name}
+              </h2>
+              <p className="font-['Titillium_Web'] text-xl text-white">
+                {user.mail}
+              </p>
+            </div>
           </div>
-          <div>
-            <h2>{user.name}</h2>
-            <p>{user.mail}</p>
+          <div className="w-[50%]">
+            <div>
+              <button className="h-48 w-48 border-dashed border-2 border-white rounded-[10px] text-white">
+                Upload
+              </button>
+            </div>
           </div>
         </div>
-        <div></div>
+        <div className="min-h-[350px] bg-gray-200 px-4 py-16">
+          <p>OLA</p>
+        </div>
       </div>
     </div>
   );
